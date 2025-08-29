766ed9e62015efa96405f326f69d21b8b22bc998
Pepedik <s2300864@edu.bc.fi>
2025-08-29 09:42:15 +0300

    Attempt counter

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,24 +1,59 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+    public int attempt = 0;
+
+    private static ButtonScript instance;
+
+    private void Awake()
+    {
+        attempt = PlayerPrefs.GetInt("Attempts", 0);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        UpdateAttemptsUI();
+    }
     public void Restart()
     {
+        attempt++;
+        PlayerPrefs.SetInt("Attempts", attempt);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("SampleScene");
         Time.timeScale = 1.0f;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+
+        UpdateAttemptsUI();
     }
     public void Menu()
     {
         SceneManager.LoadScene("MainMenu");
+        PlayerPrefs.SetInt("Attempts", 0);
     }
     public void Game()
     {
+        PlayerPrefs.SetInt("Attempts", 0);
         SceneManager.LoadScene("SampleScene");
         Time.timeScale = 1.0f;
     }
     public void Quit()
     {
         Application.Quit();
+    }
+    private void UpdateAttemptsUI()
+    {
+        if (text != null)
+            text.text = "Attempts: " + attempt;
+    }
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
